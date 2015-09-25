ea01425fa582902d41f69531eeedc6fbeda2df9f
Josefine Rutgerson <josefinerutgerson@gmail.com>
2015-09-25 11:41:17 +0000

    Added a animation with jquery

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,5 +1,21 @@
+$(document).ready(function() {
 
-    
-    
-    console.log("Hej Josse! from Monstret");
-    
+    setInterval(
+        function() {
+            $('#sailboat').animate({
+                'margin-left': '80%'
+            }, 5000);
+            $('#sailboat').animate({
+                'margin-left': '0%'
+            }, 5000);
+        },2000);
+
+});
+
+
+
+
+
+
+
+console.log("Hej Josse! from Monstret");
