37447e8310b0d4ea15c119e891771901075bd9b5
Josefine Rutgerson <josefinerutgerson@gmail.com>
2015-09-28 19:43:49 +0000

    started with assig 3

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -48,3 +48,17 @@ var printArg = function (a, b, c) {
 console.log(printArg("Josse", "is", "awesome"));
  
  /* Uppgift 3*/
+ 
+ 
+ var gissaTalet = prompt("Hi, I'm thinking about a number between 1 and 100, can you guess what it is?");
+ var computerChoice = Math.floor( Math.random() * 101)+1;
+ if (gissaTalet === computerChoice) {
+     alert("Correct! Well done you.");
+ }
+ else if (gissaTalet > computerChoice) {
+     prompt("Nope, your number " + gissaTalet + " is to high, try again Hehehe");
+ }
+ else if (gissaTalet < computerChoice) {
+     prompt("Sorry, your number " + gissaTalet + " is to low hehehe, try again shall we?");
+     
+ }
