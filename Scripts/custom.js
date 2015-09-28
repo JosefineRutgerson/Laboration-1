33fd0e0be46f0abb60ab13786a2dd39c59cfbb17
Josefine Rutgerson <josefinerutgerson@gmail.com>
2015-09-28 17:02:33 +0000

    done with assigment 2

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -16,20 +16,35 @@ $(document).ready(function() {
 /* Labb 4 */
 
 
-console.log("Hello world");
+/* console.log("Hello world");
 window.alert("Hello world");
 
-alert("rad 1" + "\n" + "rad 2");
+alert("rad 1" + "\n" + "rad 2"); */
 
+ /* Uppgift 2*/
 
-var tal1 = 2;
-var tal2 = 4;
-var tal3 = 6;
-var tal4 = 8;
+function calcMedeltal(m){
+    var tal1 = 2;
+    var tal2 = 4;
+    var tal3 = 6;
+    var tal4 = 8;
+    return m(tal1, tal2, tal3, tal4);
+}
 
-
-var medelTal = function (tal1, tal2, tal3, tal4) {
-    
+var medelTal = function (x, y, z, v) {
+    return ((x+y+z+v) /4);
     
 }; 
 
+var medel = calcMedeltal(medelTal);
+
+console.log(medel);  /* Function medeltal uppg: 2.1 */
+
+/* Uppgift 2.1*/
+
+var printArg = function (a, b, c) {
+   return a + "," + b + "," + c; 
+};
+console.log(printArg("Josse", "is", "awesome"));
+ 
+ /* Uppgift 3*/
