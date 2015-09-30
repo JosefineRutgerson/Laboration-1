7123940557eee99e8f75e2c5bf93da4985e63551
Josefine Rutgerson <josefinerutgerson@gmail.com>
2015-09-30 11:31:32 +0000

    done with uppgift 6 Math in labb 4

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -168,3 +168,20 @@ function convertString(s) {
 
 
 convertString("jag adoRE JoSSe");
+
+
+/* Uppgift 6 Math */
+
+
+
+function triangle(){
+    var a = +prompt("Ange längden på kateter 1:");
+    var aU = Math.pow(a,2);
+    var b = +prompt("Ange längden på kateter 2:")
+    var bU = Math.pow(b,2);
+    var cU = aU + bU;
+    var c = Math.sqrt(cU);
+    console.log(c);
+}
+
+triangle();
