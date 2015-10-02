021e72928acb561c1b644a6ffb1a71ca4c0bdfb5
Josefine Rutgerson <josefinerutgerson@gmail.com>
2015-10-02 20:44:32 +0000

    labb 5 validation ongoing

--- Scripts/Validation.js
+++ Scripts/Validation.js
@@ -1,9 +1,33 @@
 $(document).ready(function() {
-    $('input').addClass('firstname');
-    
-    $('input').focus(function() {
-         $(this).css('outline-color','#37cd80');
-    });
+   var form = $("#form_validation");
+   var firstName = $("#firstname");
+   var firstnameInfo = $("#firstnameinfo");
+   var lastName = $("#lastname");
+   var lastnameInfo = $("#lastnameinfo");
+   var zipCode = $("#zipcode");
+   var zipcodeInfo = $("#zipcodeinfo");
+   var eMail = $("#email");
+   var emailInfo = $("emailinfo");
+   
+   
+   
+
+        
+   function validateFirstname() {
+       if (firstName.val().length < 5) {
+           firstName.addClass("error");
+           firstnameInfo.text("Your name isn't that short, make it 5 characters or more.");
+           firstnameInfo.addClass("error");
+           
+       } else {
+           firstName.removeClass("error");
+           firstnameInfo.text("Write your firstname.");
+           firstnameInfo.removeClass("error");
+           
+       }
+   }
+   validateFirstname();
 });
+
 
 
